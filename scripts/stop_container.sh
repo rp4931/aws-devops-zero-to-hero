257110d4f6d92036f53050fb01076e638a18d596
rp4931 <126714442+rp4931@users.noreply.github.com>
2024-02-20 14:52:53 -0500

    temp

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-containerID=$(docker ps | awk -F" " '{print $1}' | tail -n 1)
-docker rm -f $containerID
+# containerID=$(docker ps | awk -F" " '{print $1}' | tail -n 1)
+# docker rm -f $containerID
+
